e71221cdbf52f04f68636b41da741a932aa572b9
Jim Buck <jim@jimbuck.io>
2025-06-27 16:00:44 -0400

    fix: add registry URL to Node.js setup step and update repository URL format in package.json

--- .github/workflows/shared.tsx
+++ .github/workflows/shared.tsx
@@ -4,7 +4,7 @@ import { Run, Step } from '../../src/github-actions';
 
 export function LintTestNode() {
 	return (<>
-		<Step name="Setup Node.js" uses="actions/setup-node@v4" with={{ 'node-version': '20.x' }} />
+		<Step name="Setup Node.js" uses="actions/setup-node@v4" with={{ 'node-version': '20.x', 'registry-url': 'https://registry.npmjs.org' }} />
 		<Run name={`Install dependencies`}>npm ci</Run>
 		<Run name={`Run Linter`}>npm run lint</Run>
 		<Run name={`Run Tests`}>npm test</Run>
